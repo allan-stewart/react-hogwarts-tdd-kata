63215f945e3fd7fc35a869e48e5d7e3c9e1da368
zhon <zhon@xputah.org>
2015-05-22 10:40:39 -0600

    Moved Catalog to components/catalog

---
+++ src/components/catalog.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import _ from "lodash";
+
+import catalogStore from "../stores/catalog-store";
+import catalogActions from "../actions/catalog-actions";
+import Course from "./course";
+
+export default class Catalog extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = catalogStore.getState();
+  }
+
+  componentDidMount() {
+    catalogStore.listen(this.onChange.bind(this));
+    catalogActions.getCatalog();
+  }
+
+  componentWillUnmount() {
+    catalogStore.unlisten(this.onChange);
+  }
+
+  onChange(state) {
+    this.setState(state);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="jumbotron">
+          <h1>Hogwarts course catalog!</h1>
+          <p>Select your wizarding classes!</p>
+        </div>
+        <div>
+          <div className="panel panel-default" >
+
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Class</th>
+                  <th>Time</th>
+                  <th>Professor</th>
+                  <th>Credits</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                { _.map(this.state.catalog, item => <Course course={item} /> ) }
+              </tbody>
+
+            </table>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
--- story-1.js
+++ story-1.js
@@ -1,11 +1,6 @@
 import React from "react";
-import _ from "lodash";
 
-//import alt from "./src/alt";
-
-import Course from "./src/components/course";
-import catalogStore from "./src/stores/catalog-store";
-import catalogActions from "./src/actions/catalog-actions";
+import Catalog from "./src/components/catalog.jsx";
 
 
 //////////////////////////
@@ -28,58 +23,6 @@ export class RegistrationResponse extends React.Component {
       }
     }
     return {message};
-  }
-}
-
-export class Catalog extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = catalogStore.getState();
-  }
-
-  componentDidMount() {
-    catalogStore.listen(this.onChange.bind(this));
-    catalogActions.getCatalog();
-  }
-
-  componentWillUnmount() {
-    catalogStore.unlisten(this.onChange);
-  }
-
-  onChange(state) {
-    this.setState(state);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="jumbotron">
-          <h1>Hogwarts course catalog!</h1>
-          <p>Select your wizarding classes!</p>
-        </div>
-        <div>
-          <div className="panel panel-default" >
-
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Class</th>
-                  <th>Time</th>
-                  <th>Professor</th>
-                  <th>Credits</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                { _.map(this.state.catalog, item => <Course course={item} /> ) }
-              </tbody>
-
-            </table>
-          </div>
-        </div>
-      </div>
-    );
   }
 }
 
