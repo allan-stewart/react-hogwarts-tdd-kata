835d4d0af02f3bac00b04f0892048e29fcd6218a
zhon <zhon@xputah.org>
2015-05-22 16:56:15 -0600

    Attempting to get jest running: Failed

---
+++ src/components/__tests__/catalog_test.js
@@ -0,0 +1,28 @@
+//jest.dontMock('../catalog.js');
+
+//import React from 'react/addons';
+//import Catalog from "../catalog";
+
+//var TestUtils = React.addons.TestUtils;
+
+describe('catalog component', function() {
+
+  it('renders properly', () => {
+    //<Catalog state={state} />
+    //var catalog = TestUtils.renderIntoDocument(
+      //<Catalog />
+    //);
+
+    //var title = TestUtils.findRenderedDOMComponentWithTag(appRoot, 'h1');
+    //var carts = TestUtils.scryRenderedComponentsWithType(appRoot, Cart);
+
+    //expect(title.getDOMNode().textContent).toEqual('My React App');
+    //expect(carts.length).toBe(1);
+    //expect(carts[0].props).toEqual({
+      //cart: state.cart
+    //});
+  });
+
+});
+
+
---
+++ test/unit/catalog.spec.js
@@ -0,0 +1,9 @@
+import should from 'should'
+
+describe('catalog component', () => {
+
+  it ('is sane', () => {
+    true.should.eql(true)
+  })
+
+})
