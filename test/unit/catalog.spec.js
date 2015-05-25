7f9e4ca01c25cdd6549bc018493224735bfc41e5
zhon <zhon@xputah.org>
2015-05-25 11:16:39 -0600

    Added mocha test for catalog

--- test/unit/catalog.spec.js
+++ test/unit/catalog.spec.js
@@ -1,9 +1,37 @@
-import should from 'should'
+import should from 'should';
+import React from 'react/addons';
 
-describe('catalog component', () => {
+import Catalog from '../../src/components/catalog'
 
-  it ('is sane', () => {
-    true.should.eql(true)
-  })
 
-})
+var TestUtils = React.addons.TestUtils;
+
+describe('catalog component', function() {
+
+  it('renders headers', () => {
+    //<Catalog state={state} />
+    var catalog = TestUtils.renderIntoDocument(
+      <Catalog />
+    );
+    var titles = TestUtils.scryRenderedDOMComponentsWithTag(catalog, 'th');
+    should(titles[0].getDOMNode().textContent).be.equal('Class');
+    should(titles[1].getDOMNode().textContent).be.equal('Time');
+    should(titles[2].getDOMNode().textContent).be.equal('Professor');
+    should(titles[3].getDOMNode().textContent).be.equal('Credits');
+  });
+
+  it('renders data', () => {
+    //<Catalog state={state} />
+    var catalog = TestUtils.renderIntoDocument(
+      <Catalog />
+    );
+    var body = TestUtils.findRenderedDOMComponentWithTag(catalog, 'tbody');
+    console.log(body.getDOMNode().textContent)
+    //should(titles[0].getDOMNode().textContent).be.equal('Class');
+    //should(titles[1].getDOMNode().textContent).be.equal('Time');
+    //should(titles[2].getDOMNode().textContent).be.equal('Professor');
+    //should(titles[3].getDOMNode().textContent).be.equal('Credits');
+  });
+
+});
+
