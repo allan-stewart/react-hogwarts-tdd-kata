83d201cf690116e71d593412efeacefb2def345a
zhon <zhon@xputah.org>
2015-05-30 15:51:35 -0600

    Fixed Course tests

--- test/unit/course.spec.js
+++ test/unit/course.spec.js
@@ -12,7 +12,7 @@ describe('course component', function() {
     name: "DADA",
     professor: "Quirinus Quirrell",
     credits: "3",
-    startTime: new Date(1992, 6, 4, 11, 30),
+    startTime: new Date(0, 0, 0, 11, 30),
   };
 
   it('renders', () => {
@@ -33,12 +33,12 @@ describe('course component', function() {
     var catalog = TestUtils.renderIntoDocument(
       <table>
         <tbody>
-          <Course course/>
+          <Course course={course}/>
         </tbody>
       </table>
     );
     var data = TestUtils.scryRenderedDOMComponentsWithTag(catalog, 'td');
-    should(data[1].getDOMNode().textContent).be.equal('11:30 am');
+    should(data[3].getDOMNode().textContent).be.equal('11:30 am');
   });
 
   it('renders nothing if no course is supplied', () => {
