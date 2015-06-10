f371b9dc5b00ba6b166dfa9ae82a52fdc298d9b1
zhon <zhon@xputah.org>
2015-06-10 09:22:41 -0600

    Starting state for course

--- src/components/course.js
+++ src/components/course.js
@@ -1,27 +1,2 @@
 import React from "react";
-import moment from "moment";
 
-export default class Course extends React.Component {
-
-  handleRegisterClick(event) {
-    event.preventDefault();
-    this.props.onRegister(this.props.course);
-  }
-
-  render() {
-    var course = this.props.course;
-    if (!course) {
-      return null;
-    }
-    return (
-      <tr>
-        <td>{course.name}</td>
-        <td>{course.professor}</td>
-        <td>{course.credits}</td>
-        <td>{moment(course.startTime).format("h:mm a")}</td>
-        { this.props.onRegister ? <td><a href="#" onClick={this.handleRegisterClick.bind(this)}>Register</a></td> : null }
-      </tr>
-    );
-  }
-
-}
