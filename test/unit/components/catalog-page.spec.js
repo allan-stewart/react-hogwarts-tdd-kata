4f4e081d0800bae3ee4f2f90b9c43e783e938fc3
zhon <zhon@xputah.org>
2015-06-11 11:47:56 -0600

    Added tests for CatalogPage

--- test/unit/components/catalog-page.spec.js
+++ test/unit/components/catalog-page.spec.js
@@ -1,11 +1,27 @@
-import {expect} from 'chai';
+import {expect, assert} from 'chai';
 import React from 'react/addons';
 
 import CatalogPage from '../../../src/components/catalog-page';
+import Catalog from '../../../src/components/catalog';
+import Alert from '../../../src/components/alert';
 
 
 var TestUtils = React.addons.TestUtils;
 
-describe('CatalogStore component', () => {
+describe('CatalogPage component', () => {
+
+  it ('has child Catalog', () => {
+    var component = TestUtils.renderIntoDocument(
+      <CatalogPage />
+    );
+    expect(TestUtils.scryRenderedComponentsWithType(component, Catalog)).to.have.length(1);
+  });
+
+  it ('has child Alert', () => {
+    var component = TestUtils.renderIntoDocument(
+      <CatalogPage />
+    );
+    expect(TestUtils.scryRenderedComponentsWithType(component, Alert)).to.have.length(1);
+  });
 
 });
