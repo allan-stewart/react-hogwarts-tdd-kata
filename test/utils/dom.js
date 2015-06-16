bf2ba26a24e038a1935ebf16002386e8d3821d04
zhon <zhon@xputah.org>
2015-06-15 21:57:21 -0600

    Remove lint errors

--- test/utils/dom.js
+++ test/utils/dom.js
@@ -1,18 +1,17 @@
-import jsdom from 'jsdom'
+import jsdom from 'jsdom';
 
-var doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
-var win = doc.defaultView
-global.document = doc
-global.window = win
-propagateToGlobal(win)
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
+const win = doc.defaultView;
+global.document = doc;
+global.window = win;
 
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
-function propagateToGlobal (window) {
-  for (var key in window) {
+function propagateToGlobal(window) {
+  for (let key in window) {
     if (!window.hasOwnProperty(key)) continue;
     if (key in global) {
       if (process.env.JSDOM_VERBOSE) {
-        console.warn("[jsdom] Warning: skipping cleanup of global['"+key+"']");
+        console.warn("[jsdom] Warning: skipping cleanup of global['" + key + "']");
       }
       continue;
     }
@@ -20,3 +19,5 @@ function propagateToGlobal (window) {
     global[key] = window[key];
   }
 }
+
+propagateToGlobal(win);
